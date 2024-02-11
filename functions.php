186ca65ed99e519da506163296f3fe83dac7b570
TeaGainz <115928301+TeaGainz@users.noreply.github.com>
2024-02-11 21:54:53 +0800

    Changed project title Name

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ echo <<<EOT
 	<body>
     <nav class="navtop">
     	<div>
-    		<h1>Home</h1>
+    		<h1>Contacts  Manager</h1>
             <a href="index.php"><i class="fas fa-home"></i>Home</a>
     		<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
     	</div>

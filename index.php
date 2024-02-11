f41459107594987daa76f30bf9504a499db7dd08
TeaGainz <115928301+TeaGainz@users.noreply.github.com>
2024-02-11 19:21:49 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Maayong bunkag mga real niggas</p>
+</div>
+
+<?=template_footer()?>

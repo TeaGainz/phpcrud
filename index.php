43bfce78946b02afe5c1a2b4df531439dbc971fc
dreamymc <school.mcbernard@gmail.com>
2024-02-19 18:38:59 +0800

    added homepage features reyes asa na ka

--- index.php
+++ index.php
@@ -1,15 +1,90 @@
 <?php
 include 'functions.php';
 // Your PHP code here.
-
-// Home Page template below.
 ?>
 
-<?=template_header('Home')?>
 
-<div class="content">
-	<h2>Home</h2>
-	<p>Welcome to the Homepage!</p>
-</div>
-
-<?=template_footer()?>
+<?= template_header('Home') ?>
+<section>
+	<div class="card-container">
+		<div class="card"><a href="hottub">
+				<div class="card--display"><i class="material-icons">hot_tub</i>
+					<h2>Hot Tub Time Machine</h2>
+				</div>
+				<div class="card--hover">
+					<h2>Hot Tub Time Machine</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie
+						pellentesque mi nec lacinia. Cras volutpat arcu sit amet elit sodales, nec volutpat velit
+						bibendum.</p>
+					<p class="link">Click to see project</p>
+				</div>
+			</a>
+			<div class="card--border"></div>
+		</div>
+	</div>
+	<div class="card-container">
+		<div class="card"><a href="worldweb">
+				<div class="card--display"><i class="material-icons">public</i>
+					<h2>World Wide Web</h2>
+				</div>
+				<div class="card--hover">
+					<h2>World Wide Web</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie
+						pellentesque mi nec lacinia. Cras volutpat arcu sit amet elit sodales, nec volutpat velit
+						bibendum.</p>
+					<p class="link">Click to see project</p>
+				</div>
+			</a>
+			<div class="card--border"></div>
+		</div>
+	</div>
+	<div class="card-container">
+		<div class="card"><a href="trainthings">
+				<div class="card--display"><i class="material-icons">train</i>
+					<h2>Why I Hate Trains</h2>
+				</div>
+				<div class="card--hover">
+					<h2>Why I Hate Trains</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie
+						pellentesque mi nec lacinia. Cras volutpat arcu sit amet elit sodales, nec volutpat velit
+						bibendum.</p>
+					<p class="link">Click to see project</p>
+				</div>
+			</a>
+			<div class="card--border"></div>
+		</div>
+	</div>
+	<div class="card-container">
+		<div class="card card--dark"><a href="androidupdate">
+				<div class="card--display"><i class="material-icons">android</i>
+					<h2>Another Android Update</h2>
+				</div>
+				<div class="card--hover">
+					<h2>Another Android Update</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie
+						pellentesque mi nec lacinia. Cras volutpat arcu sit amet elit sodales, nec volutpat velit
+						bibendum.</p>
+					<p class="link">Click to see project</p>
+				</div>
+			</a>
+			<div class="card--border"></div>
+		</div>
+	</div>
+	<div class="card-container">
+		<div class="card card--dark"><a href="phonesoff">
+				<div class="card--display"><i class="material-icons">ring_volume</i>
+					<h2>Why Phones Are So Loud</h2>
+				</div>
+				<div class="card--hover">
+					<h2>Why Phones Are So Loud</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie
+						pellentesque mi nec lacinia. Cras volutpat arcu sit amet elit sodales, nec volutpat velit
+						bibendum.</p>
+					<p class="link">Click to see project</p>
+				</div>
+			</a>
+			<div class="card--border"></div>
+		</div>
+	</div>
+</section>
+<?= template_footer() ?>

97636b2b6a965af7ddccd0c65feecee3a4b22edd
Rey Reyes <j.reyes.536490@umindanao.edu.ph>
2024-02-26 20:05:18 +0800

    Change create contacts

--- create.php
+++ create.php
@@ -14,7 +14,8 @@ if (!empty($_POST)) {
     $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
 
     // Validate email
-    function is_valid_email($email) {
+    function is_valid_email($email)
+    {
         // Regex pattern for valid email address
         $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
         return preg_match($pattern, $email);
@@ -31,13 +32,14 @@ if (!empty($_POST)) {
         $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)');
         $stmt->execute([$id, $name, $email, $phone, $title, $created]);
         $msg = 'Created Successfully!';
+        header('Location: read.php');
     }
 }
 ?>
 
-<?=template_header('Create')?>
+<?= template_header('Create') ?>
 
-<div class="content update">
+<!-- <div class="content update">
     <h2>Create Contact</h2>
     <form action="create.php" method="post">
         <label for="id">ID</label>
@@ -51,12 +53,50 @@ if (!empty($_POST)) {
         <label for="title">Title</label>
         <label for="created">Created</label>
         <input type="text" name="title" placeholder="Employee" id="title">
-        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i')?>" id="created">
+        <input type="datetime-local" name="created" value="<?= date('Y-m-d\TH:i') ?>" id="created">
         <input type="submit" value="Create">
     </form>
-    <?php if ($msg): ?>
-    <p><?=$msg?></p>
+    <?php if ($msg) : ?>
+        <p><?= $msg ?></p>
+    <?php endif; ?>
+</div> -->
+
+<div class="form-container">
+    <form action="create.php" class="my-form" method="post">
+        <div class="form-branding">
+            <h2>Create Contact</h2>
+        </div>
+        <div class="form-input-content">
+            <label for="id">ID</label>
+            <input class="customInput" readonly type="text" name="id" placeholder="26" value="auto" id="id" required autofocus />
+        </div>
+        <div class="form-input-content">
+            <label for="name">Name</label>
+            <input class="customInput" type="text" name="name" placeholder="John Doe" id="name" required autofocus />
+        </div>
+        <div class="form-input-content">
+            <label for="email">Email</label>
+            <input class="customInput" type="text" name="email" placeholder="johndoe@example.com" id="email" required autofocus />
+        </div>
+        <div class="form-input-content">
+            <label for="phone">Phone</label>
+            <input class="customInput" type="text" name="phone" placeholder="2025550143" id="phone" required />
+        </div>
+        <div class="form-input-content">
+            <label for="title">Title</label>
+            <input class="customInput" type="text" name="title" placeholder="Employee" id="title" />
+        </div>
+        <div class="form-input-content">
+            <label for="created">Created</label>
+            <input class="customInput" readonly type="datetime-local" name="created" value="<?= date('Y-m-d\TH:i') ?>" id="created" required />
+        </div>
+        <div class="form-input-content">
+            <input class="inputHoverinput customInput" type="submit" value="Create" />
+        </div>
+    </form>
+    <?php if ($msg) : ?>
+        <p><?= $msg ?></p>
     <?php endif; ?>
 </div>
 
-<?=template_footer()?>
+<?= template_footer() ?>

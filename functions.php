6da35ee9b274b080cd92d03957777ee40540bcb4
dreamymc <school.mcbernard@gmail.com>
2024-02-26 17:55:14 +0800

    added a new background

--- functions.php
+++ functions.php
@@ -1,26 +1,29 @@
 <?php
-function pdo_connect_mysql() {
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'phpcrud';
-    try {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-    } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
-    }
+function pdo_connect_mysql()
+{
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
+	$DATABASE_NAME = 'phpcrud';
+	try {
+		return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+	} catch (PDOException $exception) {
+		// If there is an error with the connection, stop the script and display the error.
+		exit('Failed to connect to database!');
+	}
 }
-function template_header($title) {
-echo <<<EOT
+function template_header($title)
+{
+	echo <<<EOT
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>$title</title>
-		<link href="CustomUpdate.css" rel="stylesheet" type="text/css">
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link href="card.css" rel="stylesheet" type="text/css">
+		<link href="body.css" rel="stylesheet" type="text/css">
+		<link href="CustomUpdate.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body>
@@ -31,12 +34,22 @@ echo <<<EOT
     		<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
     	</div>
     </nav>
+    <div class='light x1'></div>
+    <div class='light x2'></div>
+    <div class='light x3'></div>
+    <div class='light x4'></div>
+    <div class='light x5'></div>
+    <div class='light x6'></div>
+    <div class='light x7'></div>
+    <div class='light x8'></div>
+    <div class='light x9'></div>
+
 EOT;
 }
-function template_footer() {
-echo <<<EOT
+function template_footer()
+{
+	echo <<<EOT
     </body>
 </html>
 EOT;
 }
-?>
